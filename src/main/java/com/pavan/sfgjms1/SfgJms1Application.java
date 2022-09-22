340f894924eba65455b1eded9987d6471671c08c
PAVAN-KUMAR23 <chpavankumarreddy1234@gmail.com>
2022-09-22 09:56:50 +0530

    project initiated

---
+++ src/main/java/com/pavan/sfgjms1/SfgJms1Application.java
@@ -0,0 +1,13 @@
+package com.pavan.sfgjms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJms1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgJms1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavan/sfgjms1/SfgJms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavan.sfgjms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
