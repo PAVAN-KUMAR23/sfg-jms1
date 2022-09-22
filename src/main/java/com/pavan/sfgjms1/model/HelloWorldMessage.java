85ccbdad9cbac4de1885c8b0547855c601bdcafe
PAVAN-KUMAR23 <chpavankumarreddy1234@gmail.com>
2022-09-22 10:12:48 +0530

    created simple pojo

---
+++ src/main/java/com/pavan/sfgjms1/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.pavan.sfgjms1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -7233351656760201634L;
+    private UUID id;
+    private String message;
+
+}
